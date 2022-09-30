b2a15be3411064d6cf29f6a35715597dfdc6adfb
Ramiz Sabovic <frostywinterr@gmail.com>
2022-09-30 11:18:34 +0200

    Create Initial Layout Component

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,10 @@
+import { PropsWithChildren } from "react";
+import Nav from "./Nav";
+
+
+const Layout = (props: PropsWithChildren) => {
+    return <>
+        <Nav />
+        {props.children}
+    </>
+}
