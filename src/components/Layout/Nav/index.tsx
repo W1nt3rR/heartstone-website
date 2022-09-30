e404a38eeed97d18afb3590e4002db23bbc43ea5
Ramiz Sabovic <frostywinterr@gmail.com>
2022-09-30 11:17:26 +0200

    Create Initial Navbar

---
+++ src/components/Layout/Nav/index.tsx
@@ -0,0 +1,10 @@
+import { PropsWithChildren } from "react"
+import style from "./nav.module.scss"
+
+const Nav = () => {
+    return <nav className={style.nav}>
+        
+    </nav>
+}
+
+export default Nav;
